b996a1d1f62ee720de1114a1c12e551cf41771b9
MacBook <348729411@qq.com>
2016-04-01 16:46:17 +0800

    ife-16

--- ife-16/task.js
+++ ife-16/task.js
@@ -16,6 +16,26 @@ var aqiData = {};
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
+    var city = document.getElementById("aqi-city-input").value;
+    var value = document.getElementById("aqi-value-input").value;
+
+    city = city.replace(/\s/g, "");
+    value = value.replace(/\s/g, "");
+
+    var cityRE = /^[\u4e00-\u9fa5]{2,}$/;
+    var valueRE = /^[1-9][0-9]*$/;
+
+    var cityTag = cityRE.test(city);
+    var valueTag = valueRE.test(value);
+    if (!cityTag) {
+        alert("请输入正确城市名称!");
+    }
+    if (!valueTag) {
+        alert("请输入正确的空气质量!")
+    }
+    if (cityTag && valueTag) {
+        aqiData[city] = value;
+    }
 
 }
 
@@ -23,6 +43,19 @@ function addAqiData() {
  * 渲染aqi-table表格
  */
 function renderAqiList() {
+    var $table = document.getElementById("aqi-table");
+    $table.innerHTML = "<td>城市</td><td>空气质量</td><td>操作</td>";
+    for (var property in aqiData) {
+        var city = property;
+        var value = aqiData[property];
+        var teg = document.createElement("tr");
+        teg.innerHTML = "<td>" + city + "</td><td>" + value + "</td><td><button class='d-btn'>删除</button></td>"
+        $table.appendChild(teg);
+    }
+    var btn = document.getElementsByClassName("d-btn");
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].onclick=delBtnHandle;
+    }
 
 }
 
@@ -40,8 +73,10 @@ function addBtnHandle() {
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
 function delBtnHandle() {
-    // do sth.
 
+
+    var delCity =this.parentNode.parentNode.firstChild.textContent;
+   delete aqiData[delCity];
     renderAqiList();
 }
 
@@ -50,6 +85,8 @@ function init() {
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
 
     // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
+    document.getElementById("add-btn").onclick = addBtnHandle;
+
 
 }
 
